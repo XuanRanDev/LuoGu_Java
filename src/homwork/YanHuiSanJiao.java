6faaae77493d984572f82a5d53d3a08207dfe68b
XuanRanDev <3135419729@qq.com>
2022-01-08 20:16:32 +0800

    原答案错误，现已更正

--- src/homwork/YanHuiSanJiao.java
+++ src/homwork/YanHuiSanJiao.java
@@ -2,31 +2,44 @@ package homwork;
 
 import java.util.Scanner;
 
-/**
- * Created By XuanRan on 2022/1/7
- */
-class YanHuiSanJiao {
-    public static final int[][] dp = new int[100][100];
-    public static int ans = 1;
+public class YanHuiSanJiao {
+
+    static int N;
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        run();
+    }
 
-        dp[1][1] = 1;
 
-        for (int i = 2; ; i++) {
-            for (int j = 1; j <= n * 2; j++) {
-                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
-                if (dp[i][j] != 0) ans++;
-                if (dp[i][j] == n) {
-                    System.out.println(ans);
-                    return;
+    public static void run() {
+        N = new Scanner(System.in).nextInt();
+        if (N == 1) System.out.println(1);
+        else {
+            long ans = (N + 1L) * N / 2 + 2;
+            for (int m = 2; m < 16; m++) {
+                int start = m * 2, end = N;
+                while (start <= end) {
+                    int mid = start + end >> 1;
+                    if (C(mid, m) == N) {
+                        ans = min(ans, (mid + 1L) * mid / 2 + m + 1);
+                        break;
+                    }
+                    if (C(mid, m) > N) end = mid - 1;
+                    else start = mid + 1;
                 }
             }
+            System.out.println(ans);
         }
+    }
 
+    public static long min(long a, long b) {
+        return a < b ? a : b;
+    }
 
-
+    public static long C(int n, int m) {
+        long num = 1;
+        for (int nm = 1; nm <= m; n--, nm++)
+            if ((num = num * n / nm) > N) return num;
+        return num;
     }
 }
