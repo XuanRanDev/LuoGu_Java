85350db4d02a9e2c7233fac273d5454603ab4c7a
XuanRanDev <3135419729@qq.com>
2022-01-23 21:57:45 +0800

    Finish P4413

---
+++ src/P4413R2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * Created By XuanRan on 2022/1/23
+ */
+class P4413R2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println((2 * m) - n);
+    }
+}
