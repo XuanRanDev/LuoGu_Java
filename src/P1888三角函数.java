0750a509eeff4b4c6974e681f24d62afafc0a7ba
XuanRanDev <3135419729@qq.com>
2022-01-26 21:18:34 +0800

    Add P1888

---
+++ src/P1888三角函数.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StreamTokenizer;
+
+/**
+ * Created By XuanRan on 2022/1/26
+ */
+class P1888三角函数 {
+    public static int[] n = new int[4];
+    public static StreamTokenizer input = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+
+        }
+    }
+}
