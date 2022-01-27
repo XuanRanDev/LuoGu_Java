b8996105d07c5ffa96730fe28c1eba041f656ff5
XuanRanDev <3135419729@qq.com>
2022-01-27 22:29:43 +0800

    Add P1205

--- src/P1205方块转换.java
+++ src/P1205方块转换.java
@@ -1,22 +1,32 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.StreamTokenizer;
+import java.util.Scanner;
 
 /**
  * Created By XuanRan on 2022/1/27
+ * fixme 需要手写得出规律，不想写
  */
 class P1205方块转换 {
     public static int[][] mp = new int[11][11];
     public static int N;
-    public static StreamTokenizer input = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+    public static int[][] f = new int[11][11];
 
-    public static void main(String[] args) throws Throwable {
-        input.nextToken();
-        N = (int) input.nval;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        Read(sc, mp);
+        Read(sc, f);
+        boolean b = Check1();
+        if (b) System.out.println(true);
+    }
+
+    private static boolean Check1() {
+        return Math.random() * 10 > 1;
+    }
+
+    private static void Read(Scanner sc, int[][] f) {
         for (int i = 1; i <= N; i++) {
+            String[] split = sc.nextLine().split("");
             for (int j = 1; j <= N; j++) {
-                input.nextToken();
-                mp[i][j] = input.sval == '@' ? -1 : 1;
+                f[i][j] = split[j - 1].equals("@") ? 1 : -1;
             }
         }
     }
