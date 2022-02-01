4fb6bb45f2925b9e9372fbf6460eb92bde832d42
XuanRanDev <3135419729@qq.com>
2022-02-01 21:16:53 +0800

    Finish B2005

---
+++ src/B2005.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * Created By XuanRan on 2022/2/1
+ */
+class B2005 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String next = sc.next();
+        System.out.println("  " + next);
+        System.out.println(" " + next + next + next);
+        System.out.println(next + next + next + next + next);
+    }
+}
