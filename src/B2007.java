e2ab75b8047387fbf6e933d0b78afb9034894913
XuanRanDev <3135419729@qq.com>
2022-02-02 21:32:21 +0800

    Add B2007

---
+++ src/B2007.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * Created By XuanRan on 2022/2/2
+ */
+class B2007 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        System.out.println(a + b);
+    }
+}
