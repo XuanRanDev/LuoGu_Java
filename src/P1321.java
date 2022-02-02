2c454641def44d204ae355ca3dff4b997e8b1ac0
XuanRanDev <3135419729@qq.com>
2022-02-02 21:22:10 +0800

    Add P1321 -2

--- src/P1321.java
+++ src/P1321.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 /**
  * Created By XuanRan on 2022/1/30
+ * fixme
  */
 class P1321 {
     public static int ans, res;
     public static int[] ans_Flag = new int[280];
+    public static int[] res_Flag = new int[280];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -17,6 +19,14 @@ class P1321 {
                 ans_Flag[i] = -1;
                 ans_Flag[i + 1] = -1;
                 ans_Flag[i + 2] = -1;
+            }
+
+            if (chars[i] == 'g' && i + 3 < chars.length && chars[i + 1] == 'i' && chars[i + 2] == 'r' && chars[i + 3] == 'l') {
+                res++;
+                res_Flag[i] = -1;
+                res_Flag[i + 1] = -1;
+                res_Flag[i + 2] = -1;
+                res_Flag[i + 3] = -1;
             }
 
         }
@@ -31,8 +41,23 @@ class P1321 {
                 ans++;
             }
 
+            if (chars[i] == 'g' && i + 3 < chars.length && chars[i + 1] != '.' && chars[i + 2] != '.' && chars[i + 3] != '.' && res_Flag[i] != -1) {
+                res++;
+            }
+            if (chars[i] == 'i' && i - 1 >= 0 && i + 2 < chars.length && chars[i - 1] != '.' && chars[i + 1] != '.' && chars[i + 2] != '.' && res_Flag[i] != -1) {
+                res++;
+            }
+            if (chars[i] == 'r' && i - 2 >= 0 && i + 1 < chars.length && chars[i - 1] != '.' && chars[i - 2] != '.' && chars[i + 1] != '.' && res_Flag[i] != -1) {
+                res++;
+            }
+
+            if (chars[i] == 'l' && i - 3 >= 0 && chars[i - 1] != '.' && chars[i - 2] != '.' && chars[i - 2] != '.' && chars[i + 1] != '.' && res_Flag[i] != -1) {
+                res++;
+            }
+
 
         }
         System.out.println(ans);
+        System.out.println(res);
     }
 }
