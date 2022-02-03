1d70d58824520e67decb9d2a70c6b1a8b22c1c0b
XuanRanDev <3135419729@qq.com>
2022-02-03 20:43:51 +0800

    Finish B2008

---
+++ src/B2008.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Created By XuanRan on 2022/2/3
+ */
+
+class B2008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long l = sc.nextLong();
+        long l2 = sc.nextLong();
+        long l3 = sc.nextLong();
+        System.out.println((l + l2) * l3);
+    }
+}
