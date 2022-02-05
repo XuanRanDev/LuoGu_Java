e6e4dcffeffbece8b11f55f55d4ce65545b19634
XuanRanDev <3135419729@qq.com>
2022-02-05 20:18:25 +0800

    Finish P1321(大水题)

--- src/P1321.java
+++ src/P1321.java
@@ -1,63 +1,33 @@
 import java.util.Scanner;
 
 /**
- * Created By XuanRan on 2022/1/30
- * fixme 未完成，不会
+ * Created By XuanRan on 2022/02/05
+ * 好像这道题我想的太复杂了...
  */
-class P1321 {
-    public static int ans, res;
-    public static int[] ans_Flag = new int[280];
-    public static int[] res_Flag = new int[280];
-
+public class P1321 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String line = sc.next();
-        char[] chars = line.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            if (chars[i] == 'b' && i + 2 < chars.length && chars[i + 1] == 'o' && chars[i + 2] == 'y') {
-                ans++;
-                ans_Flag[i] = -1;
-                ans_Flag[i + 1] = -1;
-                ans_Flag[i + 2] = -1;
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        char[] chars = str.toCharArray();
+        int boyCount = 0, girlCount = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (chars[i] == 'b') {
+                boyCount++;
+            } else if (chars[i] == 'o' && chars[i - 1] != 'b') {
+                boyCount++;
+            } else if (chars[i] == 'y' && chars[i - 1] != 'o') {
+                boyCount++;
+            } else if (chars[i] == 'g') {
+                girlCount++;
+            } else if (chars[i] == 'i' && chars[i - 1] != 'g') {
+                girlCount++;
+            } else if (chars[i] == 'r' && chars[i - 1] != 'i') {
+                girlCount++;
+            } else if (chars[i] == 'l' && chars[i - 1] != 'r') {
+                girlCount++;
             }
-
-            if (chars[i] == 'g' && i + 3 < chars.length && chars[i + 1] == 'i' && chars[i + 2] == 'r' && chars[i + 3] == 'l') {
-                res++;
-                res_Flag[i] = -1;
-                res_Flag[i + 1] = -1;
-                res_Flag[i + 2] = -1;
-                res_Flag[i + 3] = -1;
-            }
-
         }
-        for (int i = 0; i < chars.length; i++) {
-            if (chars[i] == 'b' && i + 2 < chars.length && chars[i + 1] != '.' && chars[i + 2] != '.' && ans_Flag[i] != -1) {
-                ans++;
-            }
-            if (chars[i] == 'o' && i - 1 >= 0 && i + 1 < chars.length && chars[i - 1] != '.' && chars[i + 1] != '.' && ans_Flag[i] != -1) {
-                ans++;
-            }
-            if (chars[i] == 'y' && i - 2 >= 0 && chars[i - 1] != '.' && chars[i - 2] != '.' && ans_Flag[i] != -1) {
-                ans++;
-            }
-
-            if (chars[i] == 'g' && i + 3 < chars.length && chars[i + 1] != '.' && chars[i + 2] != '.' && chars[i + 3] != '.' && res_Flag[i] != -1) {
-                res++;
-            }
-            if (chars[i] == 'i' && i - 1 >= 0 && i + 2 < chars.length && chars[i - 1] != '.' && chars[i + 1] != '.' && chars[i + 2] != '.' && res_Flag[i] != -1) {
-                res++;
-            }
-            if (chars[i] == 'r' && i - 2 >= 0 && i + 1 < chars.length && chars[i - 1] != '.' && chars[i - 2] != '.' && chars[i + 1] != '.' && res_Flag[i] != -1) {
-                res++;
-            }
-
-            if (chars[i] == 'l' && i - 3 >= 0 && chars[i - 1] != '.' && chars[i - 2] != '.' && chars[i - 2] != '.' && chars[i + 1] != '.' && res_Flag[i] != -1) {
-                res++;
-            }
-
-
-        }
-        System.out.println(ans);
-        System.out.println(res);
+        System.out.println(boyCount);
+        System.out.println(girlCount);
     }
 }
