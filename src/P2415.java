ee7f1a0395de58df87a44685916b069e0c86c523
XuanRanDev <3135419729@qq.com>
2022-02-07 20:46:13 +0800

    Finish P2415

---
+++ src/P2415.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * Created By XuanRan on 2022/2/7
+ */
+class P2415 {
+    public static int num;
+    public static int he;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] split = sc.nextLine().split(" ");
+        for (int i = 0; i < split.length; i++) {
+            he += Integer.parseInt(split[i]);
+            num++;
+        }
+        System.out.println((he) * QuickPow(2, num - 1));
+    }
+
+
+    private static long QuickPow(int num, int pf) {
+        long res = 1;
+
+        while (pf > 0) {
+
+            if ((pf & 1) == 1) res *= num;
+            pf >>= 1;
+            num *= num;
+
+        }
+        return res;
+    }
+}
