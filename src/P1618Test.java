960d3b88a55f8209b03c83697f4cc15a967d72f1
XuanRanDev <3135419729@qq.com>
2022-02-09 19:56:25 +0800

    Add P1618Test

---
+++ src/P1618Test.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * Created By XuanRan on 2022/2/9
+ */
+public class P1618Test {
+    @Test
+    public void TestRepeat() {
+        System.out.println(P1618.CheckRepeat(12345));
+        System.out.println(P1618.CheckRepeat(12315));
+    }
+
+}
