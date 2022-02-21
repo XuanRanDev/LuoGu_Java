90626ee002ad4da7b04b7fb8955dad95cb5b9674
XuanRanDev <3135419729@qq.com>
2022-02-21 13:08:37 +0800

    Add P4924(好难啊）

--- src/P4924.java
+++ src/P4924.java
@@ -7,40 +7,43 @@ import java.io.StreamTokenizer;
  * Created By XuanRan on 2022/2/10
  */
 class P4924 {
-    public static int N, M;
+    public static StreamTokenizer input = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+    public static int[][] mp = new int[510][510];
+    public static int[] num = new int[510 * 510 + 100];
     public static int x, y, r, z;
-    public static int[][] mp = new int[550][550];
-    public static StreamTokenizer streamTokenizer = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+    public static int N, M;
+    public static int index = 1;
+    public static int MAXN = 510 * 510;
 
     public static void main(String[] args) {
         N = nextInt();
         M = nextInt();
-        init();
-        for (int i = 1; i <= M; i++) {
-            run(nextInt(), nextInt(), nextInt(), nextInt());
-        }
-    }
-
-    private static void run(int x, int y, int r, int z) {
-    }
-
-    private static void init() {
-        int num = 1;
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                mp[i][j] = num++;
+                num[index] = index++;
+                mp[i][j] = nextInt();
             }
         }
+
+        for (int i = 1; i <= 4; i++) {
+            x = nextInt();
+            y = nextInt();
+            r = nextInt();
+            z = nextInt();
+            
+        }
+
     }
 
+
     public static int nextInt() {
+
         try {
-            streamTokenizer.nextToken();
-            return (int) streamTokenizer.nval;
+            input.nextToken();
+            return (int) input.nval;
         } catch (IOException e) {
-            System.exit(1);
             e.printStackTrace();
         }
-        return 0;
+        return 1;
     }
 }
