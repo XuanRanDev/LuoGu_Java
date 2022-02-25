64232ead8b579ecf6f82e6c2efd11dcbc6c22109
XuanRanDev <3135419729@qq.com>
2022-02-25 21:55:29 +0800

    Finish B2016

---
+++ src/B2016.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * Created By XuanRan on 2022/2/25
+ */
+class B2016 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double l = sc.nextDouble();
+        System.out.println((long) l);
+    }
+}
